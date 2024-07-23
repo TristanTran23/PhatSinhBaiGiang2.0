350738d60e8876ca8b1762c6d5a88669a886fadf
Easyyy <phunguyensm@gmail.com>
2024-07-23 14:26:27 +0700

    turn error encoded part to vietnamese without signs

--- PhatSinhBaiGiang/Controllers/PhatSinhBaiGiangGPTController.cs
+++ PhatSinhBaiGiang/Controllers/PhatSinhBaiGiangGPTController.cs
@@ -16,7 +16,7 @@ namespace PhatSinhBaiGiang.Controllers
         // GET: PhatSinhBaiGiang
         public ActionResult Index()
         {
-            
+
             return View();
         }
         [HttpPost]
